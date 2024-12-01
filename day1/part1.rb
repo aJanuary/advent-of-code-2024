a5fa09cfa13b971da89fef09e72d6822aae5dbe3
Andrew January <andrewjanuary@gmail.com>
2024-12-01 12:41:06 +0000

    Day 1: Initial pass
    My initial pass for day 1, without any thought or refactoring

---
+++ day1/part1.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+left, right = $stdin.each_line.map {|l| l.chomp.split(/\s+/).map(&:to_i) }.transpose
+total = left.sort.zip(right.sort).map {|x, y| (x - y).abs}.sum
+
+puts total
