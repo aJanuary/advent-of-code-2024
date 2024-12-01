6684c17fd3426eacc5e3d9ec544fafe9f3ecf770
Andrew January <andrewjanuary@gmail.com>
2024-12-01 13:02:30 +0000

    Day 1: Simplify reading of file

--- day1/part1.rb
+++ day1/part1.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-left, right = $stdin.each_line.map {|l| l.chomp.split(/\s+/).map(&:to_i) }.transpose
+left, right = $stdin.each_line.map {|l| l.split.map(&:to_i) }.transpose
 total = left.sort.zip(right.sort).map {|x, y| (x - y).abs}.sum
 
 puts total
--- day1/part2.rb
+++ day1/part2.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
-left, right = $stdin.each_line.map {|l| l.chomp.split(/\s+/).map(&:to_i) }.transpose
+left, right = $stdin.each_line.map {|l| l.split.map(&:to_i) }.transpose
 total = left.map {|x| x * right.count(x)}.sum
+
 puts total
