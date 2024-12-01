8a65ff41bb9ee3fb91a92d633d81981e9d9d0548
Andrew January <andrewjanuary@gmail.com>
2024-12-01 13:03:06 +0000

    Day 1: Precompute counts of right column
    Rather than counting each time, which can duplicate work when something
    appears in the left multiple times, precompute a tally of the right
    column.

--- day1/part2.rb
+++ day1/part2.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 left, right = $stdin.each_line.map {|l| l.split.map(&:to_i) }.transpose
-total = left.map {|x| x * right.count(x)}.sum
+tally = right.tally
+total = left.map {|x| x * (tally[x] || 0)}.sum
 
 puts total
