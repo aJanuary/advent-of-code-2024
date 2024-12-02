a25efd41a9e0b7dac1109c96d336353ebf495947
Andrew January <andrewjanuary@gmail.com>
2024-12-02 09:24:02 +0000

    Day 2: Test a report in a single pass
    Rather than iterating through the report 3 times (once to calculate
    diffs, once to check for magnitude, and once to check for direction),
    just iterate through it once.
    
    Note that for part 2, we're still iterating a report multiple times to
    test with each level removed, but each of _those_ tests now only
    iterates once.

--- day2/part1.rb
+++ day2/part1.rb
@@ -2,8 +2,11 @@
 
 safe_reports = $stdin.each_line.map do |report_str|
   levels = report_str.split.map(&:to_i)
-  diffs = levels.each_cons(2).map {|a, b| b - a}
-  diffs.all? {|diff| diff.abs >= 1 && diff.abs <= 3} && (diffs.all? {|diff| diff < 0} || diffs.all? {|diff| diff > 0})
+  dir = levels[1] - levels[0] < 0
+  diffs = levels.each_cons(2).all? do |a, b|
+    diff = b - a
+    diff.abs >= 1 && diff.abs <= 3 && diff < 0 == dir
+  end
 end
 
 puts safe_reports.count(true)
--- day2/part2.rb
+++ day2/part2.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 
-safe_reports = $stdin.each_line.each_with_index.map do |report_str, i|
+safe_reports = $stdin.each_line.map do |report_str|
   levels = report_str.split.map(&:to_i)
   (0...levels.size).any? do |i|
     with_level_removed = levels[0...i] + levels[i+1..-1]
-    diffs = with_level_removed.each_cons(2).map {|a, b| b - a}
-    diffs.all? {|diff| diff.abs >= 1 && diff.abs <= 3} && (diffs.all? {|diff| diff < 0} || diffs.all? {|diff| diff > 0})
+    dir = with_level_removed[1] - with_level_removed[0] < 0
+    diffs = with_level_removed.each_cons(2).all? do |a, b|
+      diff = b - a
+      diff.abs >= 1 && diff.abs <= 3 && diff < 0 == dir
+    end
   end
 end
 
