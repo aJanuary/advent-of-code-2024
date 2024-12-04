e1127ff503e02337dea27239fd566ebb5f95f7e8
Andrew January <andrewjanuary@gmail.com>
2024-12-04 10:39:14 +0000

    Day 4: Use deltas for part 1
    Rather than harcoding different horiz/vert/diag methods, use deltas for
    the directions.

--- day4/part1.rb
+++ day4/part1.rb
@@ -1,41 +1,18 @@
 #!/usr/bin/env ruby
 
-def is_horiz(wordsearch, x, y, needle)
-  needle.chars.each_with_index.all? do |c, i|
-    wordsearch.dig(y, x + i) == c
-  end
-end
-
-def is_vert(wordsearch, x, y, needle)
-  needle.chars.each_with_index.all? do |c, i|
-    wordsearch.dig(y + i, x) == c
-  end
-end
-
-def is_diag_dr(wordsearch, x, y, needle)
-  needle.chars.each_with_index.all? do |c, i|
-    wordsearch.dig(y + i, x + i) == c
-  end
-end
-
-def is_diag_dl(wordsearch, x, y, needle)
-  needle.chars.each_with_index.all? do |c, i|
-    # Need to protect against negative indices, which will look from the end
-    # of the array.
-    x >= i && wordsearch.dig(y + i, x - i) == c
-  end
-end
-
 wordsearch = $stdin.each_line.map(&:chomp).map(&:chars)
 
-needle = 'XMAS'
 xs = (0...wordsearch.first.length).to_a
 ys = (0...wordsearch.length).to_a
+deltas = [-1, 0, 1].product([-1, 0, 1]).filter {|dx, dy| dx != 0 || dy != 0}
 count = xs.product(ys).map do |x, y|
-  [:is_horiz, :is_vert, :is_diag_dr, :is_diag_dl].map do |method|
-    (send(method, wordsearch, x, y, needle) ? 1 : 0) +
-      (send(method, wordsearch, x, y, needle.reverse) ? 1 : 0)
-  end.sum
+  deltas.count do |dx, dy|
+    'XMAS'.chars.each_with_index.all? do |c, i|
+      cx = x + (dx * i)
+      cy = y + (dy * i)
+      cx >= 0 && cy >= 0 && wordsearch.dig(cy, cx) == c
+    end
+  end
 end.sum
 
 puts count
