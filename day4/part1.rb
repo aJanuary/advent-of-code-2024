494301c8bb6adc950684f569182dedd7f328a06e
Andrew January <andrewjanuary@gmail.com>
2024-12-04 10:11:34 +0000

    Day 4: Refactor part 1

--- day4/part1.rb
+++ day4/part1.rb
@@ -1,47 +1,41 @@
 #!/usr/bin/env ruby
 
-def count_horiz(wordsearch, word)
-  wordsearch.map do |row|
-    row.each_cons(word.size).count {|slice| slice.join == word}
-  end.sum
-end
-
-def count_vert(wordsearch, word)
-  count_horiz(wordsearch.transpose, word)
-end
-
-def count_diag_dr(wordsearch, word)
-  count = 0
-  (0..(wordsearch.length - word.length)).each do |start_y|
-    (0..(wordsearch[0].length - word.length)).each do |start_x|
-      count += 1 if word.chars.each_with_index.all? do |c, i|
-        wordsearch[start_y + i][start_x + i] == c
-      end
-    end
+def is_horiz(wordsearch, x, y, needle)
+  needle.chars.each_with_index.all? do |c, i|
+    wordsearch.dig(y, x + i) == c
   end
-  count
 end
 
-def count_diag_dl(wordsearch, word)
-  count = 0
-  (0..(wordsearch.length - word.length)).each do |start_y|
-    ((word.length-1)..wordsearch[0].length).each do |start_x|
-      count += 1 if word.chars.each_with_index.all? do |c, i|
-        wordsearch[start_y + i][start_x - i] == c
-      end
-    end
+def is_vert(wordsearch, x, y, needle)
+  needle.chars.each_with_index.all? do |c, i|
+    wordsearch.dig(y + i, x) == c
   end
-  count
 end
 
-def count_matches(wordsearch, needle)
-  count_horiz(wordsearch, needle) + count_horiz(wordsearch, needle.reverse) +
-    count_vert(wordsearch, needle) + count_vert(wordsearch, needle.reverse) +
-    count_diag_dr(wordsearch, needle) + count_diag_dr(wordsearch, needle.reverse) +
-    count_diag_dl(wordsearch, needle) + count_diag_dl(wordsearch, needle.reverse)
+def is_diag_dr(wordsearch, x, y, needle)
+  needle.chars.each_with_index.all? do |c, i|
+    wordsearch.dig(y + i, x + i) == c
+  end
+end
+
+def is_diag_dl(wordsearch, x, y, needle)
+  needle.chars.each_with_index.all? do |c, i|
+    # Need to protect against negative indices, which will look from the end
+    # of the array.
+    x >= i && wordsearch.dig(y + i, x - i) == c
+  end
 end
 
 wordsearch = $stdin.each_line.map(&:chomp).map(&:chars)
-total = count_matches(wordsearch, 'XMAS')
 
-puts total
+needle = 'XMAS'
+xs = (0...wordsearch.first.length).to_a
+ys = (0...wordsearch.length).to_a
+count = xs.product(ys).map do |x, y|
+  [:is_horiz, :is_vert, :is_diag_dr, :is_diag_dl].map do |method|
+    (send(method, wordsearch, x, y, needle) ? 1 : 0) +
+      (send(method, wordsearch, x, y, needle.reverse) ? 1 : 0)
+  end.sum
+end.sum
+
+puts count
