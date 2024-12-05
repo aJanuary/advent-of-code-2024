171d70a3429391e6fa560ad6a6c83c17c240dccc
Andrew January <andrewjanuary@gmail.com>
2024-12-05 09:13:23 +0000

    Day 4: Tighten code for part 2 into a single pass
    Rather than iterating through the list multiple times to get to the
    middle page for each incorrect update, do that in a single pass.

--- day5/part2.rb
+++ day5/part2.rb
@@ -7,18 +7,9 @@ ordering_rules = ordering_str
   .transform_values {|v| v.map(&:last).flatten}
 updates = update_str.split("\n").map {|line| line.split(",").map(&:to_i)}
 
-sorted = updates.map do |update|
-  update.sort do |a, b|
-    if (ordering_rules[a] || []).include?(b)
-      -1
-    elsif (ordering_rules[b] || []).include?(a)
-      1
-    else
-      0
-    end
-  end
-end
-
-corrected_updates = sorted.filter {|line| !updates.include?(line)}
-answer = corrected_updates.map {|u| u[u.size / 2]}.sum
+answer = updates.filter_map do |update|
+  sorted = update.sort {|a, b| (ordering_rules[a] || []).include?(b) ? -1 : 1}
+  next if sorted == update
+  sorted[sorted.size / 2]
+end.sum
 puts answer
