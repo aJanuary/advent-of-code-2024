f5d79601f18062e6a6e2ca827a49bfbfdefdcd4e
Andrew January <andrewjanuary@gmail.com>
2024-12-07 08:54:20 +0000

    Day 7: Use recursion to speed up part 2
    By moving to a recursive algorithm it is easier to make optimisations
    such as returning early if the total is already too big.

--- day7/part2.rb
+++ day7/part2.rb
@@ -1,19 +1,33 @@
 #!/usr/bin/env ruby
 
+def is_valid(calibration)
+  def _is_valid(answer, nums, total)
+    return answer == total if nums.empty?
+    return false if total > answer
+
+    n, *rest = nums
+
+    return true if _is_valid(answer, rest, total + n)
+    return true if _is_valid(answer, rest, total * n)
+
+    mag = Math.log10(n).to_i + 1
+    concat = ((10**mag) * total) + n
+    return true if _is_valid(answer, rest, concat)
+
+    false
+  end
+
+  n, *rest = calibration[:nums]
+  _is_valid(calibration[:answer], rest, n)
+end
+
 calibrations = $stdin.each_line.map do |line|
   answer, *nums = line.scan(/\d+/).map(&:to_i)
   { answer: answer, nums: nums }
 end
 
 valid_calibrations = calibrations.filter do |calibration|
-  potential_answers = calibration[:nums].reduce([]) do |a, b|
-    if a == []
-      [b]
-    else
-      a.flat_map {|n| [n + b, n * b, "#{n}#{b}".to_i]}
-    end
-  end
-  potential_answers.include?(calibration[:answer])
+  is_valid(calibration)
 end
 
 puts valid_calibrations.map {|c| c[:answer]}.sum
