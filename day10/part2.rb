9d4b6e68faf24160da4a6d7b940e61bfcac03256
Andrew January <andrewjanuary@gmail.com>
2024-12-10 09:42:47 +0000

    Day 10: Add Grid abstraction over the map
    Rather than dealing with indexing [coord.y][coord.x], create a Grid
    class that can be indexed by the coord, as well as a few other utility
    functions such as each_pos to yield each coordinate in the grid. This
    makes the code easier to read.

--- day10/part2.rb
+++ day10/part2.rb
@@ -6,19 +6,53 @@ Coord = Struct.new(:x, :y) do
   end
 end
 
-def rank_trail(map, pos)
-  return 1 if map[pos.y][pos.x] == 9
+Grid = Struct.new(:map) do
+  def [](pos)
+    map[pos.y][pos.x]
+  end
 
-  [
-    Coord.new(0, -1),
-    Coord.new(1, 0),
-    Coord.new(0, 1),
-    Coord.new(-1, 0)
-  ].map do |d|
+  def []=(pos, value)
+    map[pos.y][pos.x] = value
+  end
+
+  def in_bounds?(pos)
+    pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height
+  end
+
+  def height
+    map.size
+  end
+
+  def width
+    map[0].size
+  end
+
+  def each_pos(&block)
+    return enum_for(:each_pos) unless block_given?
+
+    height.times do |y|
+      width.times do |x|
+        yield Coord.new(x, y)
+      end
+    end
+  end
+end
+
+CARDINAL_DIRS = [
+  Coord.new(0, -1),
+  Coord.new(1, 0),
+  Coord.new(0, 1),
+  Coord.new(-1, 0)
+]
+
+def rank_trail(map, pos)
+  return 1 if map[pos] == 9
+
+  CARDINAL_DIRS.map do |d|
     new_pos = pos + d
-    if new_pos.x < 0 || new_pos.x >= map[0].size || new_pos.y < 0 || new_pos.y >= map.size
+    if !map.in_bounds?(new_pos)
       0
-    elsif map[new_pos.y][new_pos.x] == map[pos.y][pos.x] + 1
+    elsif map[new_pos] == map[pos] + 1
       rank_trail(map, new_pos)
     else
       0
@@ -26,11 +60,8 @@ def rank_trail(map, pos)
   end.sum
 end
 
-map = $stdin.readlines.map {|l| l.chomp.split('').map(&:to_i)}
+map = Grid.new($stdin.readlines.map {|l| l.chomp.split('').map(&:to_i)})
 
-starts = map.each_with_index.flat_map do |row, y|
-  row.each_with_index.select {|cell, x| cell == 0}.map {|cell, x| Coord.new(x, y)}
-end
-
+starts = map.each_pos.filter {|pos| map[pos] == 0}
 ranks = starts.map {|start| rank_trail(map, start)}
 puts ranks.sum
